4ac76a857be447c6f92104e24e5b7c85eb7c1e1a
Ricardo Freitas Araujo <ricardo.freitas@prestadores.uhgbrasil.com.br>
2018-10-30 16:05:02 -0300

    Aula 15 - término

---
+++ YouLearn.Shared/Settings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YouLearn.Shared
+{
+    public static class Settings
+    {
+        public static string ConnectionString = @"Server=localhost\SQLEXPRESS;Database=YouLearnCurso;Trusted_Connection=True;";
+    }
+}
